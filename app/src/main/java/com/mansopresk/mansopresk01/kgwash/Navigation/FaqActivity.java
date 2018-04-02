d7284e3ff35286b144a1396880ad63d3880eb53d
prathyusha-sunkaranam <prathyu565@gmail.com>
2018-04-02 11:53:42 +0530

    Nav2 commit

---
+++ app/src/main/java/com/mansopresk/mansopresk01/kgwash/Navigation/FaqActivity.java
@@ -0,0 +1,15 @@
+package com.mansopresk.mansopresk01.kgwash.Navigation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mansopresk.mansopresk01.kgwash.R;
+
+public class FaqActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faq);
+    }
+}
--- app/src/main/java/com/mansopresk/mansopresk01/kgwash/Navigation/NavigationMainActivity.java
+++ app/src/main/java/com/mansopresk/mansopresk01/kgwash/Navigation/NavigationMainActivity.java
@@ -1,4 +1,4 @@
-package com.mansopresk.mansopresk01.kgwash;
+package com.mansopresk.mansopresk01.kgwash.Navigation;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.mansopresk.mansopresk01.kgwash.R;
 
 public class NavigationMainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
