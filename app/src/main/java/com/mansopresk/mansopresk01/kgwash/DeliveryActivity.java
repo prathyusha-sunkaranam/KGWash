c9377d2f0d5e4180e322455e998b7dc830740ffa
prathyusha-sunkaranam <prathyu565@gmail.com>
2018-04-09 12:32:42 +0530

    navc3 commit

--- app/src/main/java/com/mansopresk/mansopresk01/kgwash/DeliveryActivity.java
+++ app/src/main/java/com/mansopresk/mansopresk01/kgwash/DeliveryActivity.java
@@ -1,7 +1,10 @@
 package com.mansopresk.mansopresk01.kgwash;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class DeliveryActivity extends AppCompatActivity {
 
@@ -10,4 +13,35 @@ public class DeliveryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delivery);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.delivery, menu);
+
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.itemorder) {
+
+
+            return true;
+        }
+
+        else if (id == R.id.itemlog){
+
+            getApplicationContext().getSharedPreferences("userdetails", 0).edit().clear().commit();
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
