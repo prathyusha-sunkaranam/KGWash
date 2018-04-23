6cf7048e504a0db2a260669094da8352dce0212e
Mansopresk21 <kalagara183@gmail.com>
2018-04-23 15:16:20 +0530

    kishore premium commit

--- app/src/main/java/com/mansopresk/mansopresk01/kgwash/Navigation/Premium.java
+++ app/src/main/java/com/mansopresk/mansopresk01/kgwash/Navigation/Premium.java
@@ -1,15 +1,21 @@
 package com.mansopresk.mansopresk01.kgwash.Navigation;
 
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.mansopresk.mansopresk01.kgwash.R;
 
 public class Premium extends AppCompatActivity {
+    TextView cost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_premium);
+
+        cost=(TextView)findViewById(R.id.basiccost);
+       cost.setPaintFlags(cost.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
     }
 }
