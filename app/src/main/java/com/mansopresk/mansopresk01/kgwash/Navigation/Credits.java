64431427f1e677b5c29a22dc6e31f6451f6f0379
prathyusha-sunkaranam <prathyu565@gmail.com>
2018-04-23 16:00:47 +0530

    imagess commit

--- app/src/main/java/com/mansopresk/mansopresk01/kgwash/Navigation/Credits.java
+++ app/src/main/java/com/mansopresk/mansopresk01/kgwash/Navigation/Credits.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.mansopresk.mansopresk01.kgwash.R;
 
@@ -14,17 +15,22 @@ public class Credits extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
-        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbartc);
-        setSupportActionBar(mToolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbarcredits);
+        mToolbar.setNavigationIcon(R.drawable.ic_arrow_back_black);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i2 = new Intent(Credits.this,NavigationMainActivity.class);
+                startActivity(i2);
+                finish();
+            }
+        });
     }
 
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
-        Intent i = new Intent(Credits.this,NavigationMainActivity.class);
-        startActivity(i);
         return true;
     }
 }
